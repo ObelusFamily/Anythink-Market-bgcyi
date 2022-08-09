783555ad0565f3c20c72783aeefca99f5ae5e4fb
joeye917 <joeyellis917@yahoo.com>
2022-08-08 20:59:17 -0700

    Implemented script to seed users, comments, and items

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,62 @@
-//TODO: seeds script should come here, so we'll be able to put some data in our local env
+require("../models/User");
+require("../models/Item");
+require("../models/Comment");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const Item = mongoose.model("Item");
+const Comment = mongoose.model("Comment");
+const slug = require("slug");
+mongoose.connect(process.env.MONGODB_URI);
+
+createUser = function (index) {
+  var user = new User();
+  user.username = `user${index}`;
+  user.email = `email${index}@gmail.com`;
+  user.setPassword(`password${index}`);
+  user.bio = "";
+  return user;
+};
+
+createItem = function (index, seller) {
+  var item = new Item();
+  item.title = `item${index}`;
+  item.description = `description${index}`;
+  item.slug = slug(item.title);
+  item.seller = seller;
+  return item;
+};
+
+createComment = function (index, seller, item) {
+  var comment = new Comment();
+  comment.body = `comment${index}`;
+  comment.seller = seller;
+  comment.item = item;
+  return comment;
+};
+
+for (let i = 0; i < 100; i++) {
+  let currUser = createUser(i);
+  currUser.save(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Successfully saved user");
+  });
+  let currItem = createItem(i, currUser);
+  currItem.save(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Successfully saved item");
+  });
+  let currComment = createComment(i, currUser, currItem);
+  currComment.save(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Successfully saved comment");
+  });
+}
